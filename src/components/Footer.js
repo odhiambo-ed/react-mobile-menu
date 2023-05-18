b1c60f35b0556f9b555f07715c90138c85eab17e
Edward Odhiambo <odhiambo.edward111@gmail.com>
2023-05-18 16:03:34 +0000

    Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+      <div className='bg-gray-200 bottom-0 absolute text-center w-[100%]'>
+          &copy; Copyright 2023
+    </div>
+  )
+}
+
+export default Footer
