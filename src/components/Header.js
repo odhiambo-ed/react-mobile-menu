87cfc9a8b6b46502a39933df01e64eab65dfdcc2
Edward Odhiambo <odhiambo.edward111@gmail.com>
2023-05-18 15:01:08 +0000

    Add Tailwind and Font Awesome

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
   return (
-      <div>
-          <h3>LOGO</h3>
-          <p>Nav</p>
-    </div>
+      <nav>
+          <h3 className='text-blue-600'>LOGO</h3>
+          <span className='text-xl'>
+              <FontAwesomeIcon
+                  icon={faBars}
+              />
+          </span>
+    </nav>
   )
 }
 
